4fc10e75f5b8fadf189a48fd8f5fdfadb7a99f4f
gopalidc <111879517+gopalidc@users.noreply.github.com>
2023-07-01 17:08:32 +0530

    Add files via upload
    import java.util.Scanner;
    
    class Adition {
        public static void main(String[] args) {
            int a, b;
            float avg;
    
            System.out.println("Enter two Numbers");
            Scanner sc = new Scanner(System.in);
            a = sc.nextInt();
            b = sc.nextInt();
            avg = (a + b);
            System.out.println("average of two numbers is :" + avg);
        }
    }

---
+++ Addition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Adition {
+    public static void main(String[] args) {
+        int a, b;
+        float avg;
+
+        System.out.println("Enter two Numbers");
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        avg = (a + b) / 2;
+        System.out.println("average of two numbers is :" + avg);
+    }
+}
